fb3d472eba5d8bac69f064360fe6252618a1dd16
nicolasbock <nicolasbock@users.noreply.github.com>
2022-10-14 03:51:33 +0000

    Deploying to gh-pages from @ Temperate-Designs/platformer-1@9dbc699a041b7430ce90b477bcf8e15de080c0b1 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "bdba820bdbf1d5c4fcec5555d60aba9d",
-"index.html": "60d3fa481efbf605c82f864fbc51b0ab",
-"/": "60d3fa481efbf605c82f864fbc51b0ab",
+"index.html": "c3c5ab8857452dec1728a55e716739fd",
+"/": "c3c5ab8857452dec1728a55e716739fd",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "9c0fbdf604f45944e8e6193e2db34551",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
